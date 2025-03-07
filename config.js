c9157daa5a17b23a7bdcedd37997ba24bb5d7bd6
PineVentas <125777524+PineVentas@users.noreply.github.com>
2025-03-07 14:03:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,14 @@ module.exports = {
   language: "es",
   ownerID: ["1044072113247633418", ""], 
   mongodbUri : "mongodb+srv://pineda:pinegra@discordbot.yavpm.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "8b65025a4b1548fca224616c2ad226cd",
+  spotifyClientSecret : "47ef18bf63d3458eb6d8b40f240e82b6",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "Pineda Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/bdnxde8k",
+  SupportServer: "https://discord.gg/tSJuJR2V4E",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
